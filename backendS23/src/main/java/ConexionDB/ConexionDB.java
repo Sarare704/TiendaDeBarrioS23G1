fb328182753d9ec526c65d11fdee988f97233afa
Joan0701 <90218242+Joan0701@users.noreply.github.com>
2021-10-04 21:44:20 +0200

    Update ConexionDB.java

--- backendS23/src/main/java/ConexionDB/ConexionDB.java
+++ backendS23/src/main/java/ConexionDB/ConexionDB.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.ArrayList;
 
 
 
@@ -21,15 +21,17 @@ public class ConexionDB {
     private Connection con;
     private Statement stmt;
     private ResultSet rs;
+    //ArrayList<String> columnas;
 
     public ConexionDB() {
 
         DB_driver = "com.mysql.jdbc.Driver";
-        db = "mitienda";//aca va el nombre de mi base de datos
+        db = "mibarrio2";//aca va el nombre de mi base de datos
         host = "localhost:3306"; //aca va mi puerto de la BD 3306
         username = "root";
-        password = "Eidy.98#";
+        password = "joanRORO1313";
         url = "jdbc:mysql://" + host + "/" + db;
+        //columnas = new ArrayList<>();
 
         try {
             Class.forName(DB_driver);
@@ -87,10 +89,10 @@ public class ConexionDB {
     }
 
     public ResultSet consultar(String nombreTabla) {
-        String query = "SELECT * FROM" + nombreTabla;
+        String query = "SELECT * FROM " + nombreTabla+";";
 
         try {
-            stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             rs = stmt.executeQuery(query);
             return rs;
         } catch (SQLException ex) {
@@ -103,6 +105,7 @@ public class ConexionDB {
             Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
+        
 
     }
 
@@ -126,7 +129,7 @@ public class ConexionDB {
     }
 
     public ResultSet consultarXID(String nombreTabla, int id) {
-        String query = "SELECT *FROM" + nombreTabla + "WHERE id" + nombreTabla + " = " + id;
+        String query = "SELECT * FROM " + nombreTabla + " WHERE id " + nombreTabla + " = " + id+";";
 
         try {
             stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
@@ -145,7 +148,7 @@ public class ConexionDB {
     }
 
     public ResultSet consultarWhere(String nombreTabla, String condiciones) {
-        String query = "SELECT *FROM" + nombreTabla + "WHERE " + condiciones;
+        String query = "SELECT * FROM " + nombreTabla + " WHERE " + condiciones+";";
 
         try {
             stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
@@ -206,46 +209,75 @@ public class ConexionDB {
     
     
 public boolean actualizar(String nombreTabla, String[] valores,int id) {
-    ArrayList<String> columnas = getColumns(nombreTabla);//Usar get columns para obtener los nombres de las columnas y realizar el metodo
-    StringBuilder query = new StringBuilder("UPDATE");
-    query.append(nombreTabla);
-    query.append(" (");
-    
-    for (int i = 0;i < columnas.size();i++){
-        query.append(columnas.get(i));
-        if (i!=(columnas.size()-1))
-            query.append(", ");
         
-    }
-    query.append(") VALUES (" );
-    for (int i = 0;i < valores.length;i++){
-        query.append(valores[i]);
-        if (i!=(valores.length-1))
-            query.append(", ");
+        ArrayList<String> columnas = getColumns(nombreTabla);//Usar get columns para obtener los nombres de las columnas y realizar el metodo
+        StringBuilder query = new StringBuilder("UPDATE");
+        query.append(nombreTabla);
+        query.append(" (");
         
+        for (int i = 0;i < columnas.size();i++){
+            query.append(columnas.get(i));
+            if (i!=(columnas.size()-1))
+                query.append(", ");
+            
+        }
+        query.append(") VALUES (" );
+        for (int i = 0;i < valores.length;i++){
+            query.append(valores[i]);
+            if (i!=(valores.length-1))
+                query.append(", ");
+            
+        }
+        query.append(" )");
+       
+        try {
+
+            stmt = con.createStatement();
+            stmt.execute(query.toString());
+            return true;
+            //return rs.getInt("id"+ nombreTabla);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } catch (RuntimeException ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        } catch (Exception ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+
+    }    
+
+ public ArrayList<String> getColumns(String nombreTabla) { // Metodo para obtener los nombres de  columnas de cualquier tabla
+        ArrayList<String> columnas = new ArrayList<>();   
+        String query = "SELECT COLUMN NAME FROM INFORMATION_SCHEMA.Columns WHERE TABLE_SCHEMA = '"+ db +"' AND TABLE_NAME ='" + nombreTabla+"' ORDER BY ORDINAL_POSITION";
+       
+        try {
+            stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = stmt.executeQuery(query);
+            
+            while(rs.next()){
+                if(!rs.getString("COLUMN NAME").equals("id"+nombreTabla))
+                columnas.add(rs.getString("COLUMN NAME"));
+                
+            }
+            return columnas;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (RuntimeException ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (Exception ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
     }
-    query.append(" )");
-   
-    try {
-
-        stmt = con.createStatement();
-        stmt.execute(query.toString());
-        return true;
-        //return rs.getInt("id"+ nombreTabla);
-
-    } catch (SQLException ex) {
-        Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
-        return false;
-    } catch (RuntimeException ex) {
-        Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
-        return false;
-    } catch (Exception ex) {
-        Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
-        return false;
-    }
-
-}        
-public boolean eliminar(String nombreTabla,int id) {
+ 
+ public boolean eliminar(String nombreTabla,int id) {
         StringBuilder query = new StringBuilder("DELETE FROM ");
         query.append(nombreTabla);
         query.append("WHERE id");
@@ -268,5 +300,8 @@ public boolean eliminar(String nombreTabla,int id) {
             return false;
         }
 
-    } 
+    }
+ 
+ 
+
 }
